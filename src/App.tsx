779e7f679be4a1def8b2f4bf16417cbff4d44898
Michał Mąka <mmaka@student.agh.edu.pl>
2026-05-01 13:10:08 +0200

    feat(curr-proj): include in app & styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero.tsx'
 import AboutBox from './components/About'
+import CurrentProjects from './components/CurrentProjects.tsx'
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
 
       <section className="about-section" aria-label="Sekcja o nas">
         <AboutBox />
+      </section>
+
+      <section className="current-projects-section" aria-label="Sekcja aktualne projekty">
+        <CurrentProjects />
       </section>
 
       <section className="section-placeholder" aria-label="Sekcja projekty">
