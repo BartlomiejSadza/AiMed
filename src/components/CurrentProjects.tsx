8ce812ff1aa6c64d91adedcbe949a1f361dd4eac
Michał Mąka <mmaka@student.agh.edu.pl>
2026-05-06 21:58:31 +0200

    feat: add avif & webp variants to images

--- src/components/CurrentProjects.tsx
+++ src/components/CurrentProjects.tsx
@@ -1,8 +1,19 @@
 import Project from './Project'
 import badanieKliniczneImg from '../assets/current_projects/badania-kliniczne.png'
+import badanieKliniczneWebp from '../assets/current_projects/badania-kliniczne.webp'
+import badanieKliniczneAvif from '../assets/current_projects/badania-kliniczne.avif'
+
 import wykrywanieKardiomegaliiImg from '../assets/current_projects/wykrywanie-kardiomegalii.png'
+import wykrywanieKardiomegaliiWebp from '../assets/current_projects/wykrywanie-kardiomegalii.webp'
+import wykrywanieKardiomegaliiAvif from '../assets/current_projects/wykrywanie-kardiomegalii.avif'
+
 import analizaObrazowMedycznychImg from '../assets/current_projects/analiza-obrazow-medycznych.png'
+import analizaObrazowMedycznychWebp from '../assets/current_projects/analiza-obrazow-medycznych.webp'
+import analizaObrazowMedycznychAvif from '../assets/current_projects/analiza-obrazow-medycznych.avif'
+
 import rozwojAplikacjiMobilnychImg from '../assets/current_projects/rozwoj-aplikacji-mobilnych.png'
+import rozwojAplikacjiMobilnychWebp from '../assets/current_projects/rozwoj-aplikacji-mobilnych.webp'
+import rozwojAplikacjiMobilnychAvif from '../assets/current_projects/rozwoj-aplikacji-mobilnych.avif'
 
 const projects = [
   {
@@ -10,24 +21,32 @@ const projects = [
     name: 'Badania Kliniczne',
     desc: 'Zrealizowano szereg badań mających na celu weryfikację skuteczności opracowanych modeli w praktyce klinicznej.',
     imgPath: badanieKliniczneImg,
+    webpPath: badanieKliniczneWebp,
+    avifPath: badanieKliniczneAvif,
   },
   {
     id: 2,
     name: 'Wykrywanie Kardiomegalii',
     desc: 'Projekt opierał się na znalezieniu i opracowaniu odpowiednich cech, które były potem wykorzystane do wyuczenia wybranych modelów do prawidłowej predykcji osoby chorej lub zdrowej.',
     imgPath: wykrywanieKardiomegaliiImg,
+    webpPath: wykrywanieKardiomegaliiWebp,
+    avifPath: wykrywanieKardiomegaliiAvif,
   },
   {
     id: 3,
     name: 'Analiza Obrazów Medycznych',
     desc: 'Badanie miało na celu przetwarzanie obrazów medycznych w celu identyfikacji strukturalnych anomalii w sercu.',
     imgPath: analizaObrazowMedycznychImg,
+    webpPath: analizaObrazowMedycznychWebp,
+    avifPath: analizaObrazowMedycznychAvif,
   },
   {
     id: 4,
     name: 'Rozwój Aplikacji Mobilnych',
     desc: 'Projektując aplikację, skupiliśmy się na użyteczności i funkcjonalności, aby ułatwić użytkownikom monitorowanie stanu zdrowia.',
     imgPath: rozwojAplikacjiMobilnychImg,
+    webpPath: rozwojAplikacjiMobilnychWebp,
+    avifPath: rozwojAplikacjiMobilnychAvif,
   },
 ]
 
@@ -67,6 +86,8 @@ function CurrentProjects() {
             name={project.name}
             desc={project.desc}
             imgPath={project.imgPath}
+            webpPath={project.webpPath}
+            avifPath={project.avifPath}
           />
         ))}
       </div>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,7 +1,23 @@
-function Project({ name, desc, imgPath }: { name: string; desc: string; imgPath: string }) {
+function Project({
+  name,
+  desc,
+  imgPath,
+  webpPath,
+  avifPath,
+}: {
+  name: string
+  desc: string
+  imgPath: string
+  webpPath: string
+  avifPath: string
+}) {
   return (
     <div>
-      <img src={imgPath} alt={name}></img>
+      <picture>
+        <source srcSet={avifPath} type="image/avif" />
+        <source srcSet={webpPath} type="image/webp" />
+        <img src={imgPath} alt={name} />
+      </picture>
       <h3>{name}</h3>
       <p>{desc}</p>
       <a href="#"> Zobacz więcej...</a>
