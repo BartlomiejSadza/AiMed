3c1acc416f7f076d581fe898eab8f821514cc0e9
Michał Mąka <mmaka@student.agh.edu.pl>
2026-05-06 20:17:41 +0200

    cleanup

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,10 @@
 function Project({ name, desc, imgPath }: { name: string; desc: string; imgPath: string }) {
   return (
     <div>
-      <img src={imgPath} alt={name + ' image'}></img>
+      <img src={imgPath} alt={name}></img>
       <h3>{name}</h3>
       <p>{desc}</p>
-      <a href=""> Zobacz więcej...</a>
+      <a href="#"> Zobacz więcej...</a>
     </div>
   )
 }
